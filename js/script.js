73e74d36776a686ca9e3c9ac06a17c42b40d352c
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-15 19:59:43 -0400

    updated README

--- js/script.js
+++ js/script.js
@@ -8,12 +8,27 @@
  * Check service worker.
  */
  if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS20-PWA-Template/sw.js", {
-    scope: "/ICS20-PWA-Template/",
+  navigator.serviceWorker.register("/ICS2O-UNIT-5-08-HTML/sw.js", {
+    scope: "/ICS2O-UNIT-5-08-HTML/",
   })
 }
 
 /**
  * Input.
  */
+
+ function buttonClicked() {
+
+  var integer1 = parseInt(document.getElementById('integer1').value)
+  var integer2 = parseInt(document.getElementById('integer2').value)
+  var answer = 0 
+  var remainder = 0
+
+  if (integer1 > 0 && integer2 > 0) {
+
+  } else if () {}
+
+  document.getElementById("output").innerHTML = "Your answer is: " + answer + "The remainder is " + remainder
+}
+
 
