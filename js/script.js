113fa7af4734b5fea2a39d5743a4078209154b3b
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-16 18:53:51 -0400

    updated code

--- js/script.js
+++ js/script.js
@@ -26,9 +26,9 @@
   var remainder = 0
 
 
-  while (currentAnswer >= 0 ) {
+  while (currentAnswer > 0 ) {
     currentAnswer = currentAnswer - integer2
-    if (currentAnswer > 0) {
+    if (currentAnswer >= 0) {
       answer++
     }
   } 
